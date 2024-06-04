cd360ed243f7e2bf6f0ac2827679f27ffb5881c8
kvtrice <katricemountford@gmail.com>
2024-06-04 15:00:57 +1000

    Added basic graph functionality using stored data using chart.js

--- tracker.js
+++ tracker.js
@@ -1,8 +1,10 @@
+let chart;
 document.addEventListener("DOMContentLoaded", () => {
-    moodTracker();
+    moodSleepTracker();
+    updateChart();
 })
 
-const moodTracker = () => {
+const moodSleepTracker = () => {
     const moodChips = document.querySelectorAll('#moodChips .chip');
     const sleepChips = document.querySelectorAll('#sleepChips .chip');
     const submitButton = document.getElementById('submit');
@@ -50,6 +52,7 @@ const moodTracker = () => {
                 sleep: parseInt(selectedSleep)
             };
             saveEntry(entry);
+            updateChart();
         } else { 
             alert("Please select both mood and sleep quality!")
         }
@@ -57,9 +60,65 @@ const moodTracker = () => {
     })
 }
 
+const updateChart = () => {
+    const ctx = document.getElementById('tracker');
+    
+    const storedData = JSON.parse(localStorage.getItem('moodSleepData')) || [];
+    const moodData = storedData.map(entry => entry.mood);
+    const sleepData = storedData.map(entry => entry.sleep);
+
+    // Create an array of number values for the # of entries stored
+    let labels = [0];
+    for (let i = 1; i <= storedData.length; i++) {
+        labels.push(`Entry ${i}`)
+    }
+
+    const data = {
+        labels: labels,
+        datasets: [{
+            label: 'Mood',
+            data: [0, ...moodData],
+            borderColor: 'rgba(75, 192, 192, 1)',
+            borderWidth: 2,
+        },
+            {
+            label: 'Sleep Quality',
+            data: [0, ...sleepData],
+            borderColor: 'rgba(153, 102, 255, 1)',
+            borderWidth: 2,
+        }]
+    };
+
+    const config = {
+        type: 'line',
+        data: data,
+        options: {
+            scales: {
+                y: {
+                    ticks: {
+                        max: 5,
+                        stepSize: 1
+                    }
+                }
+            }
+        }
+    };
+
+    if (chart) {
+        chart.destroy();
+    }
+
+    chart = new Chart(ctx, config);
+};
+
 const saveEntry = (entry) => { 
     let data = JSON.parse(localStorage.getItem('moodSleepData')) || [];
         data.push(entry);
         localStorage.setItem('moodSleepData', JSON.stringify(data));
 }
 
+const deleteStoredDataButton = document.getElementById('deleteData');
+deleteStoredDataButton.addEventListener('click', () => { 
+        localStorage.removeItem('moodSleepData');
+        updateChart();
+    })
